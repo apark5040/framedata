7550b13747e87e9160b3a3190afa394d1463ccef
Andrew Park <apark11278@gmail.com>
2018-11-13 14:35:19 -0500

    First commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = process.env.PORT || 3001;
+const mongoose = require("mongoose");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/framedata");
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
